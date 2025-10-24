22ff26f0757ba80fd0af6c03fdbd0b58df046135
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2025-10-24 07:11:22 +0000

    Remove temporary models and update pipeline tasks in copilotTest and p3 pipelines.

--- folder1/pipelines/copilotTest.py
+++ folder1/pipelines/copilotTest.py
@@ -1,6 +1,24 @@
 with DAG():
-    copilotTest__reformat_customer_city = Task(
-        task_id = "copilotTest__reformat_customer_city", 
-        component = "Model", 
-        modelName = "copilotTest__reformat_customer_city"
+    new_table = Task(
+        task_id = "new_table", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "new_table", "sourceType" : "Table", "sourceName" : "ayush_demos.demos", "alias" : ""}
+    )
+    newtable = Task(
+        task_id = "newtable", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "newtable", "sourceType" : "Table", "sourceName" : "ayush_demos.demos", "alias" : ""}
+    )
+    customer = Task(
+        task_id = "customer", 
+        component = "Dataset", 
+        table = {"name" : "customer", "sourceType" : "Table", "sourceName" : "ayush_demos.demos"}
+    )
+    initial_table = Task(
+        task_id = "initial_table", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "initial_table", "sourceType" : "Table", "sourceName" : "ayush_demos.demos", "alias" : ""}
     )
--- folder1/pipelines/p3.py
+++ folder1/pipelines/p3.py
@@ -3,8 +3,6 @@ Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@g
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Config = Config, Schedule = Schedule, SensorSchedule = SensorSchedule):
-    p3__student_records = Task(task_id = "p3__student_records", component = "Model", modelName = "p3__student_records")
-    p3__dummytable_data = Task(task_id = "p3__dummytable_data", component = "Model", modelName = "p3__dummytable_data")
     run_pipeline_p1 = Task(
         task_id = "run_pipeline_p1", 
         component = "Pipeline", 
