77ace2c5507e944e99edac05f01bf03d286c7776
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2025-10-29 08:48:13 +0000

    Add new job `job11` with Bash script tasks and configure DAG orchestration

---
+++ folder1/jobs/job11/code/g_ayush_test_sql_orch_job11/tasks/Script_1.py
@@ -0,0 +1,6 @@
+from g_ayush_test_sql_orch_job11.utils import *
+
+def Script_1():
+    from airflow.operators.bash import BashOperator
+
+    return BashOperator(task_id = "Script_1", bash_command = "print(\"abc\")", )
